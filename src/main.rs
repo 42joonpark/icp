3beb9daadaa2da7cfdfcda921a83f0caf9780825
joonpark <joonpark@Rapide.local>
2022-01-04 18:58:31 +0900

    cli_42_info: writer, logger add

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use std::io::{BufReader, BufRead};
+use anyhow::{Context, Result};
+use log::{info, warn, debug};
 use std::fs::File;
+use std::io::{self, BufReader, BufRead, Write};
 use structopt::StructOpt;
 
 #[derive(StructOpt)]
@@ -12,9 +14,14 @@ struct Cli {
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+	env_logger::init();
 	let args = Cli::from_args();
-	let f = File::open(&args.path)?;
+	let path = args.path.as_os_str().to_str().unwrap();
+	let f = File::open(&args.path)
+			.with_context(|| format!("could not read file {}", path))?;
 
+	let stdout = io::stdout();
+	let mut writer = stdout.lock();
 	let mut reader = BufReader::new(f);
 	loop {
 		let mut line = String::new();
@@ -23,8 +30,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 			break;
 		}
 		if line.contains(&args.pattern) {
-			println!("{}", line);
+			writeln!(writer, "{}", line)
+			.with_context(|| format!("could not write to writebuf"))?;
 		}
 	}
+	debug!("Main End");
 	Ok(())
 }
