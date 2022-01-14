21b14e95ff2106b67844d1fa141dec3358e373a7
bagmeg <42.4.joonpark@gmail.com>
2022-01-15 05:16:04 +0900

    add run()

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,53 @@
-use reqwest::header::AUTHORIZATION;
-use serde::{Deserialize};
-use std::{error, env};
-use log::{debug, warn};
-use anyhow::{Result};
+use anyhow::Result;
 use dotenv;
+use log::debug;
+use reqwest::header::AUTHORIZATION;
+use std::io::{self, Write};
+use std::{env, error};
 
 pub mod authorize;
 pub mod info_struct;
-use authorize::{AccessToken, check};
+pub mod make_json;
+use authorize::check;
 use info_struct::me::Me;
+use make_json::*;
 
-async fn run(ac_token: AccessToken) -> Result<(), Box<dyn error::Error>> {
-	dotenv::dotenv().expect("Failed to read .env file!!");
-    let client = reqwest::Client::new();
-    let response = client
-        .get("https://api.intra.42.fr/v2/me")
-        .header(AUTHORIZATION, format!("Bearer {}", ac_token.access_token))
-        .send()
-        .await
-        .unwrap();
-
-    match response.status() {
-        reqwest::StatusCode::OK => {
-			debug!("run(): StatusCode::OK.");
+async fn run() -> Result<(), Box<dyn error::Error>> {
+    let reader = io::stdin();
+    loop {
+        let mut line = String::new();
+        print!("42_cli > ");
+        io::stdout().flush()?;
+        let bytes = reader.read_line(&mut line);
+        match bytes {
+            // unwrap Result<>
+            Err(error) => {
+                eprintln!("{}", error);
+                continue;
+            }
+            Ok(size) => {
+                if size == 0 {
+                    println!("bye!");
+                    break;
+                }
+            }
         }
-        reqwest::StatusCode::UNAUTHORIZED => {
-            warn!("unauthorized!!");
+        let command = line.trim().to_uppercase();
+        debug!("COMMAND: {}", command);
+        match command.as_str() {
+            "ME" => {
+                my_info().await?;
+            }
+            "HELP" | "COMMAND" => {}
+            "QUIT" => {
+                println!("bye!!!");
+                break;
+            }
+            _ => {}
         }
-        _ => {
-            panic!("uh oh! something unexpected happened.");
-        }
-    };
-
-	let tmp = response.text().await?;
-	println!("{}", tmp);
-
-	Ok(())
-}
-
-fn jsonize_vec<'a, T>(text: &'a str) -> Result<Vec<T>, serde_json::Error> 
-    where T: Deserialize<'a>
-{
-    let camp: Vec<T> = serde_json::from_str(text)?;
-    Ok(camp)
-}
-
-fn jsonize<'a, T>(text: &'a str) -> Result<T, serde_json::Error> 
-    where T: Deserialize<'a>
-{
-    let camp: T = serde_json::from_str(text)?;
-    Ok(camp)
+        println!("{}", line);
+    }
+    Ok(())
 }
 
 async fn my_info() -> Result<(), Box<dyn error::Error>> {
@@ -60,11 +58,11 @@ async fn my_info() -> Result<(), Box<dyn error::Error>> {
         ("grant_type", "client_credentials"),
         ("client_id", client_id.as_str()),
     ];
-	let access_token = env::var("access_token").unwrap();
+    let access_token = env::var("access_token").unwrap();
     let response = client
         .get("https://api.intra.42.fr/v2/me")
         .header(AUTHORIZATION, format!("Bearer {}", access_token))
-		.form(&params)
+        .form(&params)
         .send()
         .await
         .unwrap();
@@ -81,23 +79,18 @@ async fn my_info() -> Result<(), Box<dyn error::Error>> {
         }
     };
 
-	let tmp = response.text().await?;
-	let my_info: Me = jsonize(tmp.as_str()).unwrap();
-	println!("{}", my_info.email);
-	Ok(())
+    let tmp = response.text().await?;
+    let my_info: Me = jsonize(tmp.as_str()).unwrap();
+    println!("{}", my_info.email);
+    Ok(())
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn error::Error>>{
-	env_logger::init();
-	// let ac_token = my_authorize().await?;
-	// info!("{}", format!("AccessToken: {}", ac_token));
-	// run(ac_token).await?;
+async fn main() -> Result<(), Box<dyn error::Error>> {
+    env_logger::init();
+    let res = check::check_token_validity().await?;
+    println!("{:?}", res);
 
-	let res = check::check_token_validity().await?;
-	println!("{:?}", res);
-
-	my_info().await?;
-
-	Ok(())
+    run().await?;
+    Ok(())
 }
