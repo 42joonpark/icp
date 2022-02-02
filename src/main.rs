8c1b9cf3883c4a245f98a24c242a3dd9c7b7cbb9
bagmeg <42.4.joonpark@gmail.com>
2022-02-02 11:29:26 +0900

    add clap command.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use log::{debug, info};
-use std::io::{self, Write};
+use clap::{crate_name, crate_version, App, Arg};
+use log::debug;
 
 pub mod authorize;
 pub mod command;
@@ -7,58 +7,34 @@ pub mod structs;
 use cli_42::CliError;
 use structs::program::Program;
 
-async fn run(prog: &mut Program) -> Result<(), CliError> {
-    let reader = io::stdin();
-    loop {
-        let mut line = String::new();
-        print!("42_cli > ");
-        io::stdout().flush()?;
-        let bytes = reader.read_line(&mut line);
-        match bytes {
-            Err(error) => {
-                eprintln!("{}", error);
-                continue;
-            }
-            Ok(bytes) => {
-                if bytes == 0 {
-                    println!("bye!");
-                    break;
-                }
-            }
+async fn run(prog: &mut Program, command: &str) -> Result<(), CliError> {
+    let cmd = command.trim().to_uppercase();
+    match cmd.as_str() {
+        "WALLET" => {
+            println!("Wallet: {}", prog.wallet().await?);
         }
-        let command = line.trim().to_uppercase();
-        info!("COMMAND: {}", command);
-        match command.as_str() {
-            "CLEAR" => command::clear(),
-            "HELP" => command::help(),
-            "EMAIL" => match prog.email().await {
-                Ok(content) => {
-                    println!("Email: {}", content);
-                }
-                Err(error) => {
-                    println!("Error... {}", error);
-                }
-            },
-            "ID" => {
-                println!("ID: {}", prog.id().await?);
+        "EMAIL" => match prog.email().await {
+            Ok(content) => {
+                println!("Email: {}", content);
             }
-            "WALLET" => {
-                println!("Wallet: {}", prog.wallet().await?);
+            Err(error) => {
+                println!("Error... {}", error);
             }
-            "POINT" => {
-                println!("Correction Point: {}", prog.correction_point().await?);
-            }
-            "LOGIN" => {
-                println!("Login: {}", prog.login().await?);
-            }
-            "CAMPUS" => {
-                prog.campus().await?;
-            }
-            "QUIT" => {
-                println!("bye!!!👋👋👋👋");
-                break;
-            }
-            _ => println!("42cli: command not found: {}", line.trim()),
+        },
+        "ID" => {
+            println!("ID: {}", prog.id().await?);
+        }
+        "POINT" => {
+            println!("Correction Point: {}", prog.correction_point().await?);
+        }
+        "LOGIN" => {
+            println!("Login: {}", prog.login().await?);
+        }
+        "CAMPUS" => {
+            prog.campus().await?;
+        }
+        _ => {
+            println!("Command not found");
         }
     }
     Ok(())
@@ -68,11 +44,20 @@ async fn run(prog: &mut Program) -> Result<(), CliError> {
 async fn main() -> Result<(), CliError> {
     env_logger::init();
 
+    let cli = App::new(crate_name!())
+        .version(crate_version!())
+        .about("42 cli")
+        .arg(
+            Arg::new("command")
+                .required(true)
+                .index(1)
+                .help("Command to run"),
+        )
+        .get_matches();
+    debug!("{}", cli.value_of("command").unwrap());
+
     let mut program = Program::new();
     program.init_program().await?;
-    debug!("**** Program Status ****\n{:#?}", program);
-
-    run(&mut program).await?;
-    debug!("**** Program Status ****\n{:#?}", program);
+    run(&mut program, cli.value_of("command").unwrap()).await?;
     Ok(())
 }
