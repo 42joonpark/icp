53875ad242d28364d638581f2acec879a8efb6ec
bagmeg <42.4.joonpark@gmail.com>
2022-02-15 15:33:55 +0900

    Added CampusUserLocation

---
+++ src/results/campus_user_location.rs
@@ -0,0 +1,100 @@
+extern crate serde_json;
+use serde::{Deserialize, Serialize};
+
+pub type CampusUserLocation = Vec<CampusUserLocationElement>;
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct CampusUserLocationElement {
+    #[serde(rename = "end_at")]
+    pub end_at: Option<String>,
+
+    #[serde(rename = "id")]
+    id: i64,
+
+    #[serde(rename = "begin_at")]
+    pub begin_at: Option<String>,
+
+    #[serde(rename = "primary")]
+    primary: bool,
+
+    #[serde(rename = "host")]
+    pub host: Option<String>,
+
+    #[serde(rename = "campus_id")]
+    campus_id: i64,
+
+    #[serde(rename = "user")]
+    user: User,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct User {
+    #[serde(rename = "id")]
+    id: i64,
+
+    #[serde(rename = "email")]
+    email: String,
+
+    #[serde(rename = "login")]
+    login: String,
+
+    #[serde(rename = "first_name")]
+    first_name: String,
+
+    #[serde(rename = "last_name")]
+    last_name: String,
+
+    #[serde(rename = "usual_full_name")]
+    usual_full_name: String,
+
+    #[serde(rename = "usual_first_name")]
+    usual_first_name: Option<String>,
+
+    #[serde(rename = "url")]
+    url: String,
+
+    #[serde(rename = "phone")]
+    phone: String,
+
+    #[serde(rename = "displayname")]
+    displayname: String,
+
+    #[serde(rename = "image_url")]
+    image_url: String,
+
+    #[serde(rename = "new_image_url")]
+    new_image_url: String,
+
+    #[serde(rename = "staff?")]
+    staff: bool,
+
+    #[serde(rename = "correction_point")]
+    correction_point: i64,
+
+    #[serde(rename = "pool_month")]
+    pool_month: String,
+
+    #[serde(rename = "pool_year")]
+    pool_year: String,
+
+    #[serde(rename = "location")]
+    location: Option<String>,
+
+    #[serde(rename = "wallet")]
+    wallet: i64,
+
+    #[serde(rename = "anonymize_date")]
+    anonymize_date: String,
+
+    #[serde(rename = "created_at")]
+    created_at: String,
+
+    #[serde(rename = "updated_at")]
+    updated_at: String,
+
+    #[serde(rename = "alumni")]
+    alumni: bool,
+
+    #[serde(rename = "is_launched?")]
+    is_launched: bool,
+}
